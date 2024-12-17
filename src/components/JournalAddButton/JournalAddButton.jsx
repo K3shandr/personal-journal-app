9ddc7d4514d9731ddb8141e247fc5ec14a51094f
K3shandr <K3shandr@gmail.com>
2024-12-17 07:51:16 +0300

    typesettings

---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,17 @@
+import './JournalAddButton.css';
+import CardButton from '../CardButton/CardButton';
+
+function JournalAddButton() {
+
+
+	return (
+		<div className='journal-add-button'>
+			<CardButton className='journal-add'>
+                Новое воспоминание
+			</CardButton>
+            
+		</div>
+	);
+}
+
+export default JournalAddButton;
